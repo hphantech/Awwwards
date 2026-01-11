68c17e2d541fd1cf6c68d26750375ec093d5186a
hphan.tech <hphan.tech@gmail.com>
2026-01-11 21:47:58 +0100

    Project setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main>
+      <h1 className='text-3xl font-bold underline'>
+        Welcome to the Awwward Awards
+      </h1>
+    </main>
+  )
+}
+
+export default App
